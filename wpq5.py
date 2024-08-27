91afba607f935c219d205074fe063e0c3ad8cf48
Katie <katie.connell.91@gmail.com>
2024-08-27 18:48:50 +0800

    added tests.yml

---
+++ wpq5.py
@@ -0,0 +1,9 @@
+def find_min(weather_data):
+    """Calculates the minimum value in a list of numbers.
+
+    Args:
+        weather_data: A list of numbers.
+    Returns:
+        The minimum value and it's position in the list. (In case of multiple matches, return the index of the *last* example in the list.)
+    """
+    pass
